c5233b6e2648e2a64ea72e315f3cc5ac01d7f9f2
Jherey <jerry4christ2004@yahoo.com>
2017-12-29 05:27:28 +0100

    Remove View button

--- view.php
+++ view.php
@@ -33,7 +33,7 @@
 				echo "<p class=\"date\">"."$signin"."</p>";
 				echo "<p class=\"date\">"."$signout"."</p>";
 
-				echo "<td><a href=\"view.php?id=$res[id]\" class=\"btn btn-success btn-md butt\"><span class=\"glyphicon glyphicon-info-sign\"></span> View</a>  <a href=\"edit.php?id=$id\" class=\"btn btn-info btn-md butt\"><span class=\"glyphicon glyphicon-edit\"></span> Edit</a>  <a href=\"delete.php?id=$id\" class=\"btn btn-danger btn-md\" onClick=\"return confirm('Are you sure you want to delete?')\"><span class=\"glyphicon glyphicon-trash\"></span> Delete</a></td>";	
+				echo "<td><a href=\"edit.php?id=$id\" class=\"btn btn-info btn-md butt\"><span class=\"glyphicon glyphicon-edit\"></span> Edit</a>  <a href=\"delete.php?id=$id\" class=\"btn btn-danger btn-md\" onClick=\"return confirm('Are you sure you want to delete?')\"><span class=\"glyphicon glyphicon-trash\"></span> Delete</a></td>";	
 			?>
 		</div>
 	</div>
